7b9e221001730a4d8f6795d3e1f1b20b5da3e751
Ashwin Kumar <Ashwin.Gururajan@uab.cat>
2021-03-03 10:21:11 +0100

    Removed /bin /buid /lib from .gitignore

---
+++ build/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/DecoderLib/AnnexBread.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/AnnexBread.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/DecoderLib/BinDecoder.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/BinDecoder.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/DecoderLib/CABACReader.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/CABACReader.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/DecoderLib/DecCu.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/DecCu.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/DecoderLib/DecLib.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/DecLib.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/DecoderLib/DecSlice.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/DecSlice.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/DecoderLib/NALread.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/NALread.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/DecoderLib/SEIread.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/SEIread.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/DecoderLib/VLCReader.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/DecoderLib/CMakeFiles/DecoderLib.dir/VLCReader.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../source/Lib/DecoderLib/."
+  "../source/Lib/CommonLib/."
+  "../source/Lib/CommonLib/.."
+  "../source/Lib/CommonLib/./x86"
+  "../source/Lib/CommonLib/../libmd5"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ashwink/VVCSoftware_VTM/build/source/Lib/CommonLib/CMakeFiles/CommonLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/AQp.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/AQp.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/BinEncoder.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/BinEncoder.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/CABACWriter.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/CABACWriter.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncAdaptiveLoopFilter.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncAdaptiveLoopFilter.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncCu.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncCu.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncGOP.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncGOP.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncHRD.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncHRD.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncLib.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncLib.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncLibCommon.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncLibCommon.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncModeCtrl.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncModeCtrl.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncReshape.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncReshape.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncSampleAdaptiveOffset.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncSampleAdaptiveOffset.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncSlice.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncSlice.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/EncTemporalFilter.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/EncTemporalFilter.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/InterSearch.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/InterSearch.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/IntraSearch.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/IntraSearch.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/NALwrite.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/NALwrite.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/RateCtrl.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/RateCtrl.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/SEIEncoder.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/SEIEncoder.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/SEIwrite.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/SEIwrite.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/VLCWriter.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/VLCWriter.cpp.o"
+  "/home/ashwink/VVCSoftware_VTM/source/Lib/EncoderLib/WeightPredAnalysis.cpp" "/home/ashwink/VVCSoftware_VTM/build/source/Lib/EncoderLib/CMakeFiles/EncoderLib.dir/WeightPredAnalysis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../source/Lib/EncoderLib/."
+  "../source/Lib/CommonLib/."
+  "../source/Lib/CommonLib/.."
+  "../source/Lib/CommonLib/./x86"
+  "../source/Lib/CommonLib/../libmd5"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ashwink/VVCSoftware_VTM/build/source/Lib/CommonLib/CMakeFiles/CommonLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
